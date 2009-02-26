7d227154edb861696cdfff66d2c0537eb6b51b4d
Johannes Rudolph <johannes_rudolph@gmx.de>
2009-02-26 11:11:14 +0100

    add option to warmup before benchmarking

--- src/test/scala/net/virtualvoid/string/Benchmark.scala
+++ src/test/scala/net/virtualvoid/string/Benchmark.scala
@@ -21,7 +21,21 @@ object Benchmark {
   val formatFactories = List(ObjectFormatter,FormatCompiler)
   
   def benchmark(times:Int,formatter:IObjectFormatter[Test]) = {
+    val startTime = System.nanoTime
+ 
+	var i = times
+	do{
+	  formatter.format(sampleObject)
+	  i = i - 1;
+	}while(i>0)
+   
+	System.nanoTime - startTime
+  }
+  
+  def benchmarkWithCompilation(times:Int,factory:IObjectFormatterFactory,format:String) = {
 	val startTime = System.nanoTime
+ 
+	val formatter = factory.formatter(classOf[Test],format)
 	
 	var i = times
 	do{
@@ -34,11 +48,20 @@ object Benchmark {
   
   def main(args:Array[String]){
 	val times = args(0).toInt
+	val warmup = args(1) == "w"
+	val withCompileTimes = args(1) == "c"
 	
 	for((formatName,format)  <- formats;
-	  	factory <- formatFactories;
-		formatter = factory.formatter(classOf[Test],format)){
-	  System.out.println(factory.getClass.getSimpleName+":"+formatName+" "+(benchmark(times,formatter)/1e6)+" ms")
-	}		
+	  	factory <- formatFactories)
+	  if (withCompileTimes) 
+		  System.out.println(factory.getClass.getSimpleName+":"+formatName+" "+(benchmarkWithCompilation(times,factory,format)/1e6)+" ms")
+	  else {
+		  val formatter = factory.formatter(classOf[Test],format)
+
+		  if (warmup)
+			  benchmark(times*10,formatter)
+		  
+		  System.out.println(factory.getClass.getSimpleName+":"+formatName+" "+(benchmark(times,formatter)/1e6)+" ms")
+	  }
   }
 }
