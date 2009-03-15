fa9e077422170e680134feff8241691d75f6aa59
Johannes Rudolph <johannes_rudolph@gmx.de>
2009-03-15 15:51:57 +0100

    ojf: update to new bytecode version with better local handling

--- src/main/scala/net/virtualvoid/string/FormatCompiler.scala
+++ src/main/scala/net/virtualvoid/string/FormatCompiler.scala
@@ -23,7 +23,7 @@ object Compiler{
   def compileGetExp[R<:List,LR<:List,T,Ret](exp:Exp
                                             ,cl:Class[T]
                                             ,retType:Class[Ret])
-                                           (f:F[R**T,LR]):F[R**Ret,LR] = 
+                                           (f:F[R**T]):F[R**Ret] = 
   exp match {
     case p@ParentExp(inner,parent) =>{
       val m = p.method(cl)
@@ -38,65 +38,52 @@ object Compiler{
   }
     
   def compileFormatElementList[R<:List,LR<:List,T<:java.lang.Object]
-                 (elements:FormatElementList,cl:Class[T])
-                 (f:F[R**StringBuilder,LR**T]):F[R**StringBuilder,LR**T] =
+                 (elements:FormatElementList,cl:Class[T],value:Local[T])
+                 (f:F[R**StringBuilder]):F[R**StringBuilder] =
     elements.elements.foldLeft(f){(frame,element) => 
-      compileElement(element,cl)(frame)}
+      compileElement(element,cl,value)(frame)}
 
   def id[X]:X=>X = x=>x
   
   def compileElement[R<:List,LR<:List,T<:java.lang.Object]
-                     (ele:FormatElement,cl:Class[T])
-                     (f:F[R**StringBuilder,LR**T])
-                     :F[R**StringBuilder,LR**T]
+                     (ele:FormatElement,cl:Class[T],value:Local[T])
+                     (f:F[R**StringBuilder])
+                     :F[R**StringBuilder]
     = ele match {
       case Literal(str) => 
         f ~ ldc(str) ~ invokemethod2(_.append(_))
       case ToStringConversion(e) =>
-        f ~ local[_0,T].load() ~
+        f ~ value.load ~
           compileGetExp(e,cl,classOf[AnyRef]) ~ 
           invokemethod1(_.toString) ~ 
           invokemethod2(_.append(_))
       case Expand(exp,sep,inner) => {
+        import Bytecode.RichOperations.foldIterator
+          
         val retType = exp.returnType(cl)
 
         if (classOf[java.lang.Iterable[_]].isAssignableFrom(retType)){
           val eleType:Class[AnyRef] = elementType(exp.genericReturnType(cl)
                                                   ,classOf[java.lang.Iterable[_]])
                                                   .asInstanceOf[Class[AnyRef]]
-          val jmpTarget =
-            f ~ 
-             local[_0,T].load() ~
-             swap ~ // save one instance of T for later
-             local[_0,T].load() ~
-             compileGetExp(exp,cl,classOf[java.lang.Iterable[AnyRef]]) ~
-             invokemethod1(_.iterator) ~
-             local[_0,java.util.Iterator[AnyRef]].store() ~
-             target
           
-          jmpTarget ~
-             local[_0,java.util.Iterator[AnyRef]].load() ~
-             invokemethod1(_.hasNext) ~
-             ifne(f =>
-               f ~
-                local[_0,java.util.Iterator[AnyRef]].load() ~
-                swap ~
-                local[_0,java.util.Iterator[AnyRef]].load() ~
-                invokemethod1(_.next) ~
-                checkcast(eleType) ~
-                local[_0,AnyRef].store() ~
-                compileFormatElementList(inner,eleType) ~
-                swap ~
-                dup ~
-                local[_0,java.util.Iterator[AnyRef]].store() ~
-                invokemethod1(_.hasNext) ~
-                ifne(f =>
-                   f~ldc(sep:jString) ~
-                    invokemethod2(_.append(_)) ~
-                    jmp(jmpTarget)) ~ //todo: introduce ifeq(thenCode,elseTarget)
-                jmp(jmpTarget)) ~
-             swap ~
-             local[_0,T].store[R**StringBuilder,LR**java.util.Iterator[AnyRef]]()
+          f ~
+            value.load ~
+            compileGetExp(exp,cl,classOf[java.lang.Iterable[AnyRef]]) ~
+            invokemethod1(_.iterator) ~
+            swap ~
+            foldIterator[R,AnyRef,StringBuilder](
+              it => 
+              	_ ~ withLocal(innerValue => compileFormatElementList(inner,eleType,innerValue)) ~
+              	  it.load ~
+              	  invokemethod1(_.hasNext) ~
+              	  ifne2(
+              	  	_ ~
+              	  	  ldc(sep) ~
+              	  	  invokemethod2(_.append(_))
+                    ,f=>f
+              	  )
+            )(scala.reflect.Manifest.classType(eleType))
         }
         else if (retType.isArray){
           val eleType:Class[AnyRef] = retType.getComponentType.asInstanceOf[Class[AnyRef]]
@@ -106,39 +93,29 @@ object Compiler{
 
           import Bytecode.RichOperations.foldArray
           
-          def swapTopWithLocal0[S<:List,L<:List,ST,LT]:F[S**ST,L**LT] => F[S**LT,L**ST] = 
-            _ ~
-            local[_0,LT].load() ~
-            swap ~
-            local[_0,ST].store() ~ id
-          
-          f ~  //sb | o
-            local[_0,T].load() ~ // sb,o
-            dup ~ //sb,o,o
+          f ~
+            value.load ~
             compileGetExp(exp,cl,retType.asInstanceOf[Class[Array[AnyRef]]]) ~
-            newInstance(classOf[StringBuilder]) ~
-            foldArray( // sb,o,index,sb,ele | array
-              // add separator if nothing was added yet 
+            dup ~
+            arraylength ~
+            bipush(1) ~
+            isub ~
+            withLocal(lastIndex =>
               _ ~
-                swap ~
-                dup ~
-                invokemethod1(_.length) ~
-                ifeq2(
-                  f=>f,
-                  _ ~ ldc(sep) ~ invokemethod2(_.append(_))
-                ) ~
-                swap ~ // sb,o,index,sb,ele | array
-
-                // format element
-                swapTopWithLocal0 ~ //sb,o,index,sb,array | ele
-                swap ~
-                compileFormatElementList(inner,eleType) ~ 
-                swap ~
-                local[_0,Array[AnyRef]].store() ~ id// sb,o,index,sb | array
-            ) ~ // sb,o,sb | array
-            swap ~ // sb,sb,o | array
-            local[_0,T].store() ~
-            invokemethod2(_.append(_))
+	            swap ~
+	            foldArray(index =>
+	              _ ~ withLocal(innerValue => compileFormatElementList(inner,eleType,innerValue)) ~
+                    lastIndex.load ~
+                    index.load ~
+                    isub ~
+                    ifne2(
+                      _ ~
+                        ldc(sep) ~
+                        invokemethod2(_.append(_))
+                      , f=>f
+                    )
+	            )
+            )
         }
         else
           throw new java.lang.Error("can only iterate over "+
@@ -150,7 +127,7 @@ object Compiler{
         if (retType == java.lang.Boolean.TYPE || 
               classOf[java.lang.Boolean].isAssignableFrom(retType)){
           f ~ 
-            local[_0,T].load() ~
+            value.load ~
             (if (retType == java.lang.Boolean.TYPE)
                compileGetExp(inner,cl,classOf[Boolean])
              else 
@@ -158,30 +135,24 @@ object Compiler{
              ~ invokemethod1(_.booleanValue)
             ) ~
             ifeq2(
-              compileFormatElementList(elses,cl),
-              compileFormatElementList(thens,cl))
+              compileFormatElementList(elses,cl,value),
+              compileFormatElementList(thens,cl,value))
         }
         else if (classOf[Option[AnyRef]].isAssignableFrom(retType)){
           val eleType = elementType(inner.genericReturnType(cl)
                                     ,classOf[Option[_]])
             .asInstanceOf[Class[AnyRef]]
           f ~
-            local[_0,T].load() ~
+            value.load ~
             compileGetExp(inner,cl,classOf[Option[AnyRef]]) ~
             dup ~
             invokemethod1(_.isDefined) ~
             ifeq2(
-              _ ~ pop ~ compileFormatElementList(elses,cl),
+              _ ~ pop ~ compileFormatElementList(elses,cl,value),
               _ ~ 
                 checkcast(classOf[Some[AnyRef]]) ~
                 invokemethod1(_.get) ~
-                local[_0,T].load() ~
-                swap ~
-                local[_0,AnyRef].store() ~
-                swap ~
-                compileFormatElementList(thens,eleType) ~
-                swap ~
-                local[_0,T].store[R**StringBuilder,LR**AnyRef]())
+                withLocal(newValue => compileFormatElementList(thens,eleType,newValue)))
         }
         else
           throw new Error("can't use "+retType+" in a conditional")
@@ -196,7 +167,7 @@ object Compiler{
           dup ~
           ldc(format) ~ 
           invokemethod2(_.applyPattern(_)) ~ pop_unit ~
-          local[_0,T].load() ~
+          value.load ~
           (f => 
             retType match {
               case x if DateClass.isAssignableFrom(x)     => 
@@ -218,9 +189,9 @@ object Compiler{
     val elements:FormatElementList = parser.parse(format)
     ASMCompiler.compile(cl)(
       _ 
-      ~ local[_0,T].store()
-      ~ newInstance(classOf[StringBuilder])
-      ~ compileFormatElementList(elements,cl)
+      ~ withLocal(value =>
+      	_ ~ newInstance(classOf[StringBuilder])
+      	  ~ compileFormatElementList(elements,cl,value))
       ~ invokemethod1(_.toString)
      )
   }
