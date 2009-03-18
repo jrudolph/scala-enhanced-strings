1999e9010e0e9e9ea302b11242fc85199eb4a01f
Johannes Rudolph <johannes_rudolph@gmx.de>
2009-03-18 10:52:16 +0100

    es: simple Expand compilation

--- src/main/scala/net/virtualvoid/string/EnhancedStringsPlugin.scala
+++ src/main/scala/net/virtualvoid/string/EnhancedStringsPlugin.scala
@@ -6,6 +6,7 @@ import nsc.Phase
 import nsc.plugins.Plugin
 import nsc.plugins.PluginComponent
 import nsc.transform.Transform
+import nsc.symtab.Flags
 
 class EnhancedStringsPlugin(val global: Global) extends Plugin {
   import global._
@@ -28,6 +29,8 @@ class EnhancedStringsPlugin(val global: Global) extends Plugin {
 	  def newTransformer(unit: CompilationUnit) = new ESTransformer
     
     class ESTransformer extends Transformer {
+		val it = ValDef(Modifiers(Flags.PARAM), "it", TypeTree(), EmptyTree)
+      
 	    /** When using <code>preTransform</code>, each node is
 	     *  visited before its children.
 	     */
@@ -47,8 +50,13 @@ class EnhancedStringsPlugin(val global: Global) extends Plugin {
 	    def compileElement(el:AST.FormatElement):Tree = el match{
 	      case AST.Literal(str) => Literal(Constant(str))
 	      case AST.ToStringConversion(exp) => Select(compileExpression(exp),"toString")
+	      case AST.Expand(exp,sep,inner) => Apply(
+	    		  							   Select(
+	    		  							     Apply(Select(compileExpression(exp),"map")
+	    		  							    		 ,List(Function(List(it),compile(inner)))),"mkString")
+                                               ,List(Literal(Constant(sep))))
 	    }
-	    def compile(els:AST.FormatElementList,tree:Tree):Tree =
+	    def compile(els:AST.FormatElementList):Tree =
 	    	els.elements.size match {
 	    	  case 0 => Literal(Constant(""))
 	    	  case 1 => compileElement(els.elements(0))
@@ -61,7 +69,7 @@ class EnhancedStringsPlugin(val global: Global) extends Plugin {
 	    def postTransform(tree: Tree): Tree = tree match {
 	      case This(qual) => {System.out.println(tree+":"+qual+qual.getClass+":"+qual.toString.length);tree}
 	      case Literal(Constant(str:String)) =>
-	          compile(EnhancedStringFormatParser.parse(str),tree)
+	          compile(EnhancedStringFormatParser.parse(str))
 	      case _ => tree
 	    }
 	
