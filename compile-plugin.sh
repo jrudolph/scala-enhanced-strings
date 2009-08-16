10cbb2ca141bffa0c82430c4a5330178ae6315ad
Johannes Rudolph <johannes_rudolph@gmx.de>
2009-08-16 12:06:27 +0200

    use scala 2.7.5

--- compile-plugin.sh
+++ compile-plugin.sh
@@ -1,3 +1,3 @@
 mvn package
 zip target/objectformatter-1.0-SNAPSHOT.jar scalac-plugin.xml
-/home/johannes/download/scala-2.7.3.final/bin/scala -Xplugin:target/objectformatter-1.0-SNAPSHOT.jar 
+scala -Xplugin:target/objectformatter-1.0-SNAPSHOT.jar 
