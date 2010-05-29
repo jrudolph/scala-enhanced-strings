da1e37898b2f012e7f3456397bfbde83ed934891
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-05-29 16:11:11 +0200

    update to scala 2.8.0.RC3

--- with-plugin.sh
+++ with-plugin.sh
@@ -1,1 +1,1 @@
-~/downloads/sources/scala-2.8.0.r19928-b20091129020233/bin/scala -Xplugin:target/objectformatter-1.0-SNAPSHOT.jar
+~/downloads/sources/scala-2.8.0.RC3/bin/scala -Xplugin:target/objectformatter-1.0-SNAPSHOT.jar 
