40676a348d3f21da684f87882c1eeacd50deb183
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-06-07 11:32:17 +0200

    whitespace cleanup

--- src/main/scala/net/virtualvoid/string/EnhancedStringsPlugin.scala
+++ src/main/scala/net/virtualvoid/string/EnhancedStringsPlugin.scala
@@ -5,7 +5,7 @@ import nsc.Global
 import nsc.Phase
 import nsc.plugins.Plugin
 import nsc.plugins.PluginComponent
-import nsc.transform.{Transform, TypingTransformers}
+import nsc.transform.{ Transform, TypingTransformers }
 import nsc.symtab.Flags
 
 class EnhancedStringsPlugin(val global: Global) extends Plugin {
@@ -14,132 +14,129 @@ class EnhancedStringsPlugin(val global: Global) extends Plugin {
   val name = "enhanced-strings"
   val description = "allows variable interpolation in strings"
   val components = List[PluginComponent](Component)
-  
-  private object Component extends PluginComponent with Transform with TypingTransformers{
-	   import global._
-	   import global.definitions._
 
-	   val global = EnhancedStringsPlugin.this.global
-	   override val runsAfter = List("parser")
-	  /** The phase name of the compiler plugin
-	   *  @todo Adapt to specific plugin.
-	   */
-	  val phaseName = "enhanced-strings"
-	
-	  def newTransformer(unit: CompilationUnit) = new ESTransformer(unit)
-    
+  private object Component extends PluginComponent with Transform with TypingTransformers {
+    import global._
+    import global.definitions._
+
+    val global = EnhancedStringsPlugin.this.global
+    override val runsAfter = List("parser")
+    /** The phase name of the compiler plugin
+     *  @todo Adapt to specific plugin.
+     */
+    val phaseName = "enhanced-strings"
+
+    def newTransformer(unit: CompilationUnit) = new ESTransformer(unit)
+
     class ESTransformer(unit: CompilationUnit) extends TypingTransformer(unit) {
-        localTyper = analyzer.newTyper(analyzer.rootContext(unit, EmptyTree, false))
-    
-		val it = ValDef(Modifiers(Flags.PARAM), "it", TypeTree(), EmptyTree)
-      
-	    /** When using <code>preTransform</code>, each node is
-	     *  visited before its children.
-	     */
-	    def preTransform(tree: Tree): Tree = tree match {
-	      case _ => tree
-	    }
-	    def compiled(els:AST.FormatElementList, pos: Position): Tree = {
-	    
-	      def startOf(e: AST.Exp): Position = pos.withPoint(pos.startOrPoint + e.pos.column)
-	      def compile(els: AST.FormatElementList): Tree = compiled(els, pos)
-	    
-	    def compileParentExpressionInner(inner:AST.Exp,outer:Tree):Tree = inner match {
-	      case AST.ParentExp(inner,parent) => compileParentExpressionInner(inner,Select(outer,parent))
-	      case _ => inner match {case AST.Exp(id) => Select(outer,id)}
-	    }
-     
+      localTyper = analyzer.newTyper(analyzer.rootContext(unit, EmptyTree, false))
+
+      val it = ValDef(Modifiers(Flags.PARAM), "it", TypeTree(), EmptyTree)
+
+      def compiled(els: AST.FormatElementList, pos: Position): Tree = {
+
+        def startOf(e: AST.Exp): Position = pos.withPoint(pos.startOrPoint + e.pos.column)
+        def compile(els: AST.FormatElementList): Tree = compiled(els, pos)
+
+        def compileParentExpressionInner(inner: AST.Exp, outer: Tree): Tree = inner match {
+          case AST.ParentExp(inner, parent) => compileParentExpressionInner(inner, Select(outer, parent))
+          case _ => inner match { case AST.Exp(id) => Select(outer, id) }
+        }
+
         def parse(code: String): Tree = {
           val unit = new CompilationUnit(new scala.tools.nsc.util.BatchSourceFile("<snippet>", code))
           val scanner = new syntaxAnalyzer.UnitParser(unit)
           scanner.expr()
-        } 
-	    def compileExpression(exp:AST.Exp):Tree = atPos(startOf(exp)) { exp match {
-	      case AST.ThisExp => Ident("it")
-	      case AST.ParentExp(inner,parent) => compileParentExpressionInner(inner,Ident(parent))
-	      case AST.ScalaExp(exp) => parse(exp)
-	      case _ => exp match { case AST.Exp(identifier) => Ident(identifier) }
-	    }}
-	    def compileElement(el:AST.FormatElement):Tree = el match{
-	      case AST.Literal(str) => Literal(Constant(str))
-	      case AST.ToStringConversion(exp) => Select(compileExpression(exp),"toString")
-	      case AST.Expand(exp,sep,inner) => Apply(
-	    		  							   Select(
-	    		  							     Apply(Select(compileExpression(exp), "map")
-	    		  							    		 ,List(Function(List(it),compile(inner)))),"mkString")
-                                               ,List(Literal(Constant(sep))))
-	      case AST.Conditional(cond,thenEls,elseEls) =>
-	        Match(Typed(compileExpression(cond), Ident("Any".toTypeName)),List(
-	          CaseDef(Apply(Ident("Some"),List(Bind("it",Ident("_")))),compile(thenEls)),
-	          CaseDef(Ident("None"),compile(elseEls)),
-	          CaseDef(Bind("it", Literal(Constant(true))), compile(thenEls)),
-	          CaseDef(Literal(Constant(false)), compile(elseEls))
-	        ))
-	    }
-	    
-	    	els.elements.size match {
-	    	  case 0 => Literal(Constant(""))
-	    	  case 1 => compileElement(els.elements(0))
-	    	  case _ =>
-	    	    // the general case:
-	    	    // compile into new StringBuilder().append(a).append(b).[...].append(z).toString
-	    	    val appender = els.elements.map(compileElement _)
-	    	                      .foldLeft(
-	    	                        Apply(Select(New(Ident("StringBuilder".toTypeName)), nme.CONSTRUCTOR), Nil)) { 
-	    	                        (a, b) =>
-	    	                          Apply(Select(a, "append"), List(b)) 
-	    	                        }
-	    	    Apply(Select(appender, "toString"), Nil)
-	    	}
         }
-        
-	    /** When using <code>postTransform</code>, each node is
-	     *  visited after its children.
-	     */
-	    def postTransform(tree: Tree): Tree = tree match {
-	      case Literal(Constant(str:String)) => 
-	        try {println(version)
-	          atPos(tree.pos)(compiled(EnhancedStringFormatParser.parse(str), tree.pos))
-	        } catch {
-	          case p:ParseException => p.printStackTrace;unit.error(tree.pos, p.getMessage);tree
-	          case e:TypeError => localTyper.reportTypeError(tree.pos, e);tree
-	        }
-	      case _ => tree
-	    }
+        def compileExpression(exp: AST.Exp): Tree = atPos(startOf(exp)) {
+          exp match {
+            case AST.ThisExp => Ident("it")
+            case AST.ParentExp(inner, parent) => compileParentExpressionInner(inner, Ident(parent))
+            case AST.ScalaExp(exp) => parse(exp)
+            case _ => exp match { case AST.Exp(identifier) => Ident(identifier) }
+          }
+        }
+        def compileElement(el: AST.FormatElement): Tree = el match {
+          case AST.Literal(str) => Literal(Constant(str))
+          case AST.ToStringConversion(exp) => Select(compileExpression(exp), "toString")
+          case AST.Expand(exp, sep, inner) => Apply(
+            Select(
+            Apply(Select(compileExpression(exp), "map")
+            , List(Function(List(it), compile(inner)))), "mkString")
+            , List(Literal(Constant(sep))))
+          case AST.Conditional(cond, thenEls, elseEls) =>
+            Match(Typed(compileExpression(cond), Ident("Any".toTypeName)), List(
+              CaseDef(Apply(Ident("Some"), List(Bind("it", Ident("_")))), compile(thenEls)),
+              CaseDef(Ident("None"), compile(elseEls)),
+              CaseDef(Bind("it", Literal(Constant(true))), compile(thenEls)),
+              CaseDef(Literal(Constant(false)), compile(elseEls))
+              ))
+        }
 
-        val ESType = "EnhancedString".toTypeName
-        val SyntaxParam = "syntax".toTermName
-	    val versionExtractor: PartialFunction[Tree, String] = { case Apply(Select(New(Ident(ESType)), nme.CONSTRUCTOR), List(AssignOrNamedArg(Ident(SyntaxParam), Literal(Constant(version: String))))) => version}
-	    
-	    def extractVersion(m: Modifiers): (Modifiers, Option[String]) = m match {
-	      case Modifiers(a, b, anns, d) =>
-	        val (version, rest) = anns.partition(versionExtractor.isDefinedAt _)
-	        
-	        (Modifiers(a, b, rest, d), version.headOption.map(versionExtractor))
-	      case _ => (m, None)
-	    }
-	    
-	    var version: Option[String] = None
-	
-	    override def transform(tree: Tree): Tree = tree match {
-	      case d@DefDef(mods, _, _, _, _, _) =>
-	        val (newMods, newVersion) = extractVersion(mods)
-	        
-	        newVersion match {
-	          case Some(v) => 
-	            val old = version
-	            version = Some(v)
-	            println("Version now "+v)
-	            val res = super.transform(tree)
-	            version = old
-	            res.asInstanceOf[DefDef].copy(mods = newMods)
-	          case None => 
-	            super.transform(tree)
-	        }
-	      case Apply(Select(New(Ident(ESType)), nme.CONSTRUCTOR), List(x)) => println(x.getClass);super.transform(tree)
-	      case _ if version.isDefined => postTransform(super.transform(tree))
-	      case _ => super.transform(tree)
-	    }
+        els.elements.size match {
+          case 0 => Literal(Constant(""))
+          case 1 => compileElement(els.elements(0))
+          case _ =>
+            // the general case:
+            // compile into new StringBuilder().append(a).append(b).[...].append(z).toString
+            val createInstance: Tree = Apply(Select(New(Ident("StringBuilder".toTypeName)), nme.CONSTRUCTOR), Nil)
+            def appendElement(a: Tree, b: Tree) = Apply(Select(a, "append"), List(b))
+
+            val appender = els.elements.map(compileElement _)
+                              .foldLeft(createInstance)(appendElement)
+
+            Apply(Select(appender, "toString"), Nil)
+        }
+      }
+
+      /** When using <code>postTransform</code>, each node is
+       *  visited after its children.
+       */
+      def postTransform(tree: Tree): Tree = tree match {
+        case Literal(Constant(str: String)) =>
+          try {
+            println(version)
+            atPos(tree.pos)(compiled(EnhancedStringFormatParser.parse(str), tree.pos))
+          } catch {
+            case p: ParseException => p.printStackTrace; unit.error(tree.pos, p.getMessage); tree
+            case e: TypeError => localTyper.reportTypeError(tree.pos, e); tree
+          }
+        case _ => tree
+      }
+
+      val ESType = "EnhancedString".toTypeName
+      val SyntaxParam = "syntax".toTermName
+      val versionExtractor: PartialFunction[Tree, String] = { case Apply(Select(New(Ident(ESType)), nme.CONSTRUCTOR), List(AssignOrNamedArg(Ident(SyntaxParam), Literal(Constant(version: String))))) => version }
+
+      def extractVersion(m: Modifiers): (Modifiers, Option[String]) = m match {
+        case Modifiers(a, b, anns, d) =>
+          val (version, rest) = anns.partition(versionExtractor.isDefinedAt _)
+
+          (Modifiers(a, b, rest, d), version.headOption.map(versionExtractor))
+        case _ => (m, None)
+      }
+
+      var version: Option[String] = None
+
+      override def transform(tree: Tree): Tree = tree match {
+        case d@DefDef(mods, _, _, _, _, _) =>
+          val (newMods, newVersion) = extractVersion(mods)
+
+          newVersion match {
+            case Some(v) =>
+              val old = version
+              version = Some(v)
+              println("Version now " + v)
+              val res = super.transform(tree)
+              version = old
+              res.asInstanceOf[DefDef].copy(mods = newMods)
+            case None =>
+              super.transform(tree)
+          }
+        case Apply(Select(New(Ident(ESType)), nme.CONSTRUCTOR), List(x)) => println(x.getClass); super.transform(tree)
+        case _ if version.isDefined => postTransform(super.transform(tree))
+        case _ => super.transform(tree)
+      }
     }
   }
 }
