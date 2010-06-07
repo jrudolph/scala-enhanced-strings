a7476404e5047f8bcdb9cb83225916561a099d57
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-06-07 12:24:16 +0200

    update script wrt sbtification

--- with-plugin.sh
+++ with-plugin.sh
@@ -1,1 +1,1 @@
-~/downloads/sources/scala-2.8.0.RC3/bin/scala -Xplugin:target/objectformatter-1.0-SNAPSHOT.jar 
+~/downloads/sources/scala-2.8.0.RC3/bin/scala -Xplugin:target/scala_2.8.0.RC3/scala-enhanced-strings_2.8.0.RC3-1.0.jar
