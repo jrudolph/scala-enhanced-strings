f7601fc00982e925dcb0238821e30d7c36f2b451
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-07-01 19:02:58 +0200

    downgrade...

--- docs/source/gen.sh
+++ docs/source/gen.sh
@@ -1,5 +1,5 @@
 SXR=/home/johannes/git/opensource/sxr/target/scala_2.8.0.RC6/sxr_2.8.0.RC6-0.2.5-SNAPSHOT.jar
-ES=../../target/scala_2.8.0.RC6/scala-enhanced-strings_2.8.0.RC6-1.0.jar
+ES=../../target/scala_2.8.0.RC6/scala-enhanced-strings_2.8.0.RC6-0.5.jar
 #SBT=../../project/boot/scala-2.7.7/org.scala-tools.sbt/sbt/0.7.4/sbt_2.7.7-0.7.4.jar
 
 scalac \
