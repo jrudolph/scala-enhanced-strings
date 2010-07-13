ade9f7275e89c9f2dc8d2c4e36705912b2308aa6
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-07-13 08:18:26 +0200

    add @EnhanceStrings annotation to the sources for IDEs which don't support compiler plugins for their presentation
    see http://groups.google.com/group/scala-enhanced-strings/browse_frm/thread/a2cd0c23b47b3dc0

---
+++ src/main/scala/net/virtualvoid/string/EnhanceStringsAnnotation.scala
@@ -0,0 +1,6 @@
+/**
+ * The annotation is defined explicitly here, so that you can add the
+ * plugin's jar file to your build dependencies if your IDE doesn't
+ * support compiler plugins for presentation.
+ */
+class EnhanceStrings(syntax: String, version: Int) extends Annotation
