099cb8365106ce76278e3898c20beb7b58940410
Johannes Rudolph <johannes_rudolph@gmx.de>
2011-09-15 10:44:51 +0200

    fix deprecation warning

--- src/main/scala/net/virtualvoid/string/EnhanceStringsAnnotation.scala
+++ src/main/scala/net/virtualvoid/string/EnhanceStringsAnnotation.scala
@@ -3,4 +3,4 @@
  * plugin's jar file to your build dependencies if your IDE doesn't
  * support compiler plugins for presentation.
  */
-class EnhanceStrings(syntax: String, version: Int) extends Annotation
+class EnhanceStrings(syntax: String, version: Int) extends scala.annotation.Annotation
