a7f17a26ea2347183bcb65642374950da765ae24
Johannes Rudolph <johannes_rudolph@gmx.de>
2011-09-15 10:44:20 +0200

    update specs dependency version

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class EnhancedStrings(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins {
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.8-SNAPSHOT" % "test"
+  val specs = "org.scala-tools.testing" % "specs_2.9.0-1" % "1.6.8" % "test"
   
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
   override def packageAction = super.packageAction dependsOn(compile, test)
