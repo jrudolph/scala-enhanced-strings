e9d1a5788369bc893fef83af45f5908fa6d8917e
Johannes Rudolph <johannes_rudolph@gmx.de>
2011-09-15 16:08:37 +0200

    update documentation copyright

--- docs/source/scala-src/Overview.scala
+++ docs/source/scala-src/Overview.scala
@@ -18,7 +18,7 @@ class ScalaEnhancedStrings {
 }
 
 /* SeS - Scala Enhanced Strings
- * Copyright 2008 - 2010 Johannes Rudolph
+ * Copyright 2008 - 2011 Johannes Rudolph
  * Visit http://github.com/jrudolph/scala-enhanced-strings
  * File bugs at http://github.com/jrudolph/scala-enhanced-strings/issues
  * Mailing list at http://groups.google.com/group/scala-enhanced-strings
